5836a26be21de1808834da827b32e958a2bb1c2d
j-vst <joao.tomio@fulltechpro.com.br>
2022-06-27 20:14:07 -0300

    Projetos atualizados para .net6.0; Os pacotes utilizados foram atualizados; Adicionado mais alguns casos de testes unitários; Adicionado stryker para realizar testes de mutação;

--- BasicCrud.Tests/Tests/PessoaTest.cs
+++ BasicCrud.Tests/Tests/PessoaTest.cs
@@ -1,6 +1,8 @@
 ﻿using BasicCrud.Core.Entities;
+using BasicCrud.Core.Entities.Mock;
 using System;
 using System.ComponentModel;
+using System.Linq;
 using Xunit;
 
 namespace BasicCrud.Tests.Tests
@@ -92,6 +94,52 @@ namespace BasicCrud.Tests.Tests
             var exp = Assert.Throws<Exception>(() => pessoa.Validate());
 
             Assert.Equal("CPF inválido", exp.Message);
+        }
+
+        [Fact]
+        [Description("O método deve retornar uma exception quando a idade for menor que zero")]
+        public void Pessoa_ShouldThrowException_WhenIdadeValueIsLessThanZero()
+        {
+            var pessoa = new Pessoa()
+            {
+                Nome = "João Vitor de Souza Tomio",
+                Idade = -1,
+                DtNascimento = DateTime.Parse("09-11-2001"),
+                Email = "joao.tomio@catolicasc.org.br",
+                CPF = "12556329920",
+                Telefone = "47996073978"
+            };
+
+            var exp = Assert.Throws<Exception>(() => pessoa.Validate());
+
+            Assert.Equal("Idade inválida", exp.Message);
+        }
+
+        [Fact]
+        [Description("O método deve retornar uma exception quando a idade for igual a zero")]
+        public void Pessoa_ShouldThrowException_WhenIdadeValueEqualsZero()
+        {
+            var pessoa = new Pessoa()
+            {
+                Nome = "João Vitor de Souza Tomio",
+                Idade = 0,
+                DtNascimento = DateTime.Parse("09-11-2001"),
+                Email = "joao.tomio@catolicasc.org.br",
+                CPF = "12556329920",
+                Telefone = "47996073978"
+            };
+
+            var exp = Assert.Throws<Exception>(() => pessoa.Validate());
+
+            Assert.Equal("Idade inválida", exp.Message);
+        }
+        [Fact]
+        [Description("O método deve retornar sucesso quando a idade for maior que zero")]
+        public void Pessoa_ShouldSucceed_WhenIdadeValueIsMoreThanZero()
+        {
+            var pessoa = PessoaMock._pessoas.FirstOrDefault();
+
+            Assert.Equal(20, pessoa.Idade);
         }
     }
 }
